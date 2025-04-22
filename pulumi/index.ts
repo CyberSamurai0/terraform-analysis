e4818b45799216516dabde18cb6c811b5059f7b3
CyberSamurai0 <colinsaumure@gmail.com>
2025-04-21 21:17:37 -0400

    Define basic Ubuntu 22.04 instance 1c/1gb

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,16 +1,55 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as vultr from "@ediri/vultr";
+import * as fs from "fs";
+import * as path from "path";
 
-vultr.getInstances({
-    filters: [{
-        name: "status",
-        values: ["active"],
-    }],
-}).then((instances) => {
-    instances.instances.forEach((instance) => {
-        pulumi.log.info(`Instance ID: ${instance.id}, Status: ${instance.status}`);
-    });
-}).catch((error) => {
-    pulumi.log.error(`Error fetching instances: ${error.message}`);
+const scriptPath = path.join(__dirname, "provision.sh");
+const scriptContent = fs.readFileSync(scriptPath, "utf-8");
+
+const ProvisionScript = new vultr.StartupScript("ProvisionScript", {
+    name: "Pulumi Demo Provisioning",
+    script: Buffer.from(scriptContent).toString("base64"),
+    type: "boot"
 });
 
+let Ubuntu: string | undefined = undefined;
+vultr.getOs({
+    filters: [{
+        name: "name",
+        values: ["Ubuntu 22.04 x64"]
+    }],
+}).then(os => {
+    Ubuntu = os.id;
+});
+
+export const instance = new vultr.Instance("pulumi-instance", {
+    // Region: Seattle, Washington
+    region: "sea",
+
+    // Instance type: 1 vCPU, 1 GB RAM, 25 GB SSD, $5.00month ($0.007/hour)
+    plan: "vc2-1c-1gb",
+
+    // Ubuntu 22.04 x64
+    osId: Ubuntu,
+
+    // Configure Hostname
+    hostname: "pulumi-instance",
+
+    // Configure Label (Display Name)
+    label: "pulumi-instance",
+
+    // Disable Automatic Backups
+    backups: "disabled",
+
+    // Disable DDOS Protection
+    ddosProtection: false,
+
+    // Disable IPv6
+    enableIpv6: false,
+
+    // Startup Script ID
+    scriptId: ProvisionScript.id,
+
+    // Define Tags for the Instance
+    tags: ["pulumi"],
+});
